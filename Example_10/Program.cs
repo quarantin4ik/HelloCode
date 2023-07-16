2bb113b233a0a0ca3f4afd48d72ce4e66f9aecf0
Pavel <pavel@example.com>
2023-07-16 21:53:29 +0300

    add ex10

---
+++ Example_10/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 11, 21, 31, 43, 15, 64, 27, 58, 98, 39, 64 };
+int n = array.Length;
+int find = 64;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+
+}
